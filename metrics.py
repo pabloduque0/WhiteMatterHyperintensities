7634b9a06c30ae136872e8c8b373abe244294266
Pablo Duque <menduke@live.com>
2018-07-09 23:33:53 +0200

    Added metrics for dice coef

---
+++ metrics.py
@@ -0,0 +1,17 @@
+from keras import backend as K
+
+def dice_coefficient(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+
+    print(y_true_f.shape)
+    print(y_pred_f.shape)
+    intersection = K.sum(y_pred_f * y_true_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coefficient_loss(y_true, y_pred):
+    return -dice_coefficient(y_true, y_pred)
+
+dice_coef = dice_coefficient
+dice_coef_loss = dice_coefficient_loss
