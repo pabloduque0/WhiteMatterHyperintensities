2f6174cfdab487e94896f9d1ba6937664cbda4ac
Pablo Duque <menduke@live.com>
2018-07-30 23:02:18 +0200

    Initial modifications to use two first datasets for training

--- runner.py
+++ runner.py
@@ -12,22 +12,27 @@ parser.get_all_images(image_paths)
 
 utrech_dataset, singapore_dataset, amsterdam_dataset = parser.get_all_images_and_labels()
 
-data_utrecht = [row[1] for row in utrech_dataset]
+t1_utrecht = [row[1] for row in utrech_dataset]
+flair_utrecht = [row[2] for row in utrech_dataset]
 labels_utrecht = [row[0] for row in utrech_dataset]
 
-data_singapore = [row[1] for row in singapore_dataset]
+t1_singapore = [row[1] for row in singapore_dataset]
+flair_singapore = [row[2] for row in singapore_dataset]
 labels_singapore = [row[0] for row in singapore_dataset]
 
 #data_amsterdam = [row[1] for row in amsterdam_dataset]
 #labels_amsterdam = [row[0] for row in amsterdam_dataset]
 
-all_data = data_utrecht + data_singapore # + list(data_amsterdam)
+all_t1 = t1_utrecht + t1_singapore # + list(data_amsterdam)
+all_flair = flair_utrecht + flair_singapore
+
 all_labels = labels_utrecht + labels_singapore # + labels_amsterdam
 
 
 slice_shape = (240, 240)
 
-data = parser.get_all_images_np(all_data, slice_shape)
+data_t1 = parser.get_all_images_np(all_t1, slice_shape)
+data_flair = parser.get_all_images_np(all_flair, slice_shape)
 labels = parser.get_all_images_np(all_labels, slice_shape, normalization=False)
 
 data = np.stack(data, axis=0)
